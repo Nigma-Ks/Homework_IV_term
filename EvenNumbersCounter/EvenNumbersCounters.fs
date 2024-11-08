3975d5949c7f8d500697ee4a15dd62d882feed3d
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-08 15:42:45 +0300

    Добавила решение 4 задачи

---
+++ EvenNumbersCounter/EvenNumbersCounters.fs
@@ -0,0 +1,11 @@
+﻿namespace EvenNumbersCounter
+
+module EvenNumbersCounters =
+    let mapCounter list = List.sum <|
+                            List.map (fun x -> if x % 2 = 0 then 1
+                                                   else 0) list
+
+    let foldCounter list = List.fold (fun acc elem -> acc + if elem % 2 = 0 then 1
+                                                                else 0) 0 list
+
+    let filterCounter list = List.length <| List.filter (fun x -> x % 2 = 0) list
