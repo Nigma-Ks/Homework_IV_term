5feb2e21433dfc91abba1e3bec09e3f70bdcfc5e
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-10 20:21:44 +0300

    Добавила решение 2 задачи с тестами

---
+++ PointFree.Tests/PointFreeTest.fs
@@ -0,0 +1,14 @@
+﻿module PointFree.Tests
+
+open NUnit.Framework
+open FsCheck
+open PointFree
+
+
+[<Test>]
+let ``Functions are equal`` () =
+    let functionResAreEq (x: int) (l: int list) = (func'1 x l = func'2 x l) &&
+                                                    (func'2 x l = func'3 x l) && (func'3 x l = func'4 x l) &&
+                                                      (func'4 x l = func'5 x l)
+    
+    Check.Quick functionResAreEq
