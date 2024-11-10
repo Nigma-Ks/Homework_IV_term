649a478d722d1c5c903bc0173bed82dc07666f0a
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-10 18:35:29 +0300

    Добавила решение первой задачи с тестами

---
+++ BracketSequence/BracketSequence.fs
@@ -0,0 +1,26 @@
+﻿namespace BracketSequence
+
+module BracketSequenceChecker =
+    let bracketList = [('(', ')'); ('[', ']'); ('{', '}')]
+
+    let isOpen (bracket: char) = bracket = '(' || bracket = '{' || bracket = '['
+
+    let isClose (bracket: char) = bracket = ']' || bracket = ')' || bracket = '}'
+
+    let relativeOpen (bracket: char) = List.find (fun x -> snd x = bracket) bracketList |> fst
+
+    let isCorrectBracketSequence (bracketString: string) =
+        let rec internalBracketSequenceChecker charList bracketsStack =
+            match charList with
+                | fstChar :: tail ->
+                    if isOpen fstChar then internalBracketSequenceChecker tail (fstChar :: bracketsStack)
+                    elif isClose fstChar then
+                        match bracketsStack with
+                            | lastBracket :: brackets ->
+                                if lastBracket = relativeOpen(fstChar) then internalBracketSequenceChecker tail brackets
+                                else false
+                            | _ -> false
+                    else internalBracketSequenceChecker tail bracketsStack
+                | [] -> if bracketsStack = [] then true
+                        else false
+        internalBracketSequenceChecker (bracketString |> Seq.toList) []
