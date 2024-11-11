526042b2a08e483b2b56ce8d0a1ac841ec1669f4
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-11 03:19:46 +0300

    Добавила решение задачи 3

---
+++ PhoneBook.Tests/PhoneBookTests.fs
@@ -0,0 +1,76 @@
+﻿module PhoneBook.Tests
+
+open NUnit.Framework
+open FsUnit
+open System.IO
+open PhoneBook
+
+let testFilePath = "testFileBase.txt"
+
+[<Test>]
+let ``Write and read test`` () =
+    File.WriteAllText(testFilePath,"")
+    let data = [({Name = "Max"; Phone = "123"}); ({Name = "Max"; Phone = "123"})]
+    writeContactsToFile data testFilePath
+    readContactsFromFile testFilePath |> should equal data
+
+[<Test>]
+let ``Add new record test`` () =
+    let data = [({Name = "Max"; Phone = "123"})]
+    let expected = ([({Name = "Vasya"; Phone = "124"}); ({Name = "Max"; Phone = "123"})], true)
+    addContactToBase "Vasya" "124" data |> should equal expected
+
+[<Test>]
+let ``Can not add record with existed in base phone or name`` () =
+    let data = [({Name = "Max"; Phone = "123"})]
+    let (newData, _) = addContactToBase "Vasya" "123" data
+    let (newNewData, _) = addContactToBase "Max" "345" newData
+    (newNewData, newData) |> should equal (data, data)
+
+[<Test>]
+let ``Finding ny name and phone test`` () =
+    let name = "Max"
+    let phone = "123"
+    let contact = ({Name = name; Phone = phone})
+    let data = [contact]
+    let res1 = findContactByNameInBase name data
+    let res2 = findContactByPhoneInBase phone data
+
+    (res1, res2) |> should equal (Some(contact), Some(contact))
+
+[<Test>]
+let ``isContactInFile test`` () =
+    let name = "Max"
+    let phone = "123"
+    File.WriteAllText(testFilePath,"")
+    let data = [({Name = name; Phone = phone})]
+    writeContactsToFile data testFilePath
+    isContactInFile name phone testFilePath |> should be True
+
+[<Test>]
+let ``isContactInBase test`` () =
+    let data = [({Name = "Max"; Phone = "123"})]
+
+    isContactInBase "Max" "123" data |> should be True
+    
+[<Test>]
+let ``Write contacts from base to file test`` () =
+    File.WriteAllText(testFilePath, "")
+    let data = [({Name = "Max"; Phone = "123"})]
+    writeContactsToFile data testFilePath
+    let contacts = [({Name = "Vasya"; Phone = "124"}); ({Name = "Max"; Phone = "123"})]
+    writePhoneBaseToFile contacts testFilePath
+    readContactsFromFile testFilePath |> should equal contacts
+
+[<Test>]
+let ``Write contacts from file to base`` () =
+    File.WriteAllText(testFilePath,"")
+    let data = [({Name = "Max"; Phone = "123"}); ({Name = "Vasya"; Phone = "124"})]
+    let contacts = [({Name = "Max"; Phone = "123"})]
+    writeContactsToFile data testFilePath
+
+    writePhoneBaseFromFile contacts testFilePath |> should equal data
+
+[<Test>]
+let ``Correct phone test`` () =
+    (isCorrectPhone "88005555", isCorrectPhone "880055d5")|> should equal (true, false)
---
+++ PhoneBook/Program.fs
@@ -0,0 +1,110 @@
+﻿open System
+open PhoneBook.PhoneBook
+
+let printInstructions() =
+    printfn "It's a phone book!"
+    printfn "You can enter command number to:"
+    printfn "1. Add new record"
+    printfn "2. Find contact by phone"
+    printfn "3. Find contact by name"
+    printfn "4. Print current phone book"
+    printfn "5. Print contacts from file"
+    printfn "6. Add contacts from file to current phone book"
+    printfn "7. Add contacts from current phone book to file"
+    printfn "8. Exit"
+
+let phoneBaseFile = "PhoneBase.txt"
+
+let stringContact contact = sprintf "%s - %s" contact.Name contact.Phone
+
+let printContactList (contactList: Contact list) =
+    if List.isEmpty contactList then 
+        printfn "There are no contacts\n"
+    else
+        printfn "n"
+        for contact in contactList do
+            printfn "%s" (stringContact contact)
+
+let rec enterCorrectPhone() =
+    printfn "Enter phone: "
+    let phone = Console.ReadLine()
+    if isCorrectPhone(phone) then phone
+    else
+        printfn "Phone was incorrect\n"
+        enterCorrectPhone()
+
+let addNewRecord (phoneBase: Contact list) =
+    printfn "To add new record enter name: "
+    let name = Console.ReadLine()
+    let phone = enterCorrectPhone()
+    match addContactToBase name phone phoneBase with
+    | (newPhoneBase, true) ->
+        printfn "Contact added successfully!\n"
+        newPhoneBase
+    | _ ->
+        printfn "Contact wasn't added: phone or name are already in phone book or\n"
+        phoneBase
+
+let printFindingRes (result: Contact option) =
+    match result with
+    | Some contact ->
+        printfn "Contact was found: %s" (stringContact contact)
+    | None ->
+        printfn "Contact wasn't found"
+
+let findPhoneByName (phoneBase: Contact list) =
+    let phone = enterCorrectPhone()
+    printFindingRes (findContactByPhoneInBase phone phoneBase)
+
+let findNameByPhone (phoneBase: Contact list) =
+    printfn "Enter name: "
+    let name = Console.ReadLine()
+    printFindingRes (findContactByNameInBase name phoneBase)
+
+let printCurrentPhoneBook (phoneBase: Contact list) =
+    printfn "Current phone book:\n"
+    printContactList phoneBase
+
+let printContactsFromFile () =
+    printfn "Contacts in file:"
+    printContactList (readContactsFromFile phoneBaseFile)
+
+let addContactsFromFile (phoneBase: Contact list) = 
+    writePhoneBaseFromFile phoneBase phoneBaseFile
+
+let addContactsToFile (phoneBase: Contact list) = 
+    writePhoneBaseToFile phoneBase phoneBaseFile
+
+let Run =
+    let rec internalRun (phoneBase: Contact list) =
+        printInstructions()
+        printfn "Enter command: "
+        let command = Console.ReadLine()
+        match command with
+        | "1" ->
+            let newPhoneBase = addNewRecord phoneBase
+            internalRun newPhoneBase
+        | "2" ->
+            findPhoneByName phoneBase
+            internalRun phoneBase
+        | "3" ->
+            findNameByPhone phoneBase
+            internalRun phoneBase
+        | "4" ->
+            printCurrentPhoneBook phoneBase
+            internalRun phoneBase
+        | "5" ->
+            printContactsFromFile()
+            internalRun phoneBase
+        | "6" ->
+            let newPhoneBase = addContactsFromFile phoneBase
+            internalRun newPhoneBase
+        | "7" ->
+            addContactsToFile phoneBase
+            internalRun phoneBase
+        | "8" ->
+            printfn "Exit"
+        | _ ->
+            printfn "Incorrect command!\n"
+            internalRun phoneBase
+    internalRun []
