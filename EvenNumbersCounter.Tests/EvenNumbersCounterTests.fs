105a837fc0fa49b5aeea5cd1fa774daa9155a346
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-25 03:28:51 +0300

    Отформатировала

--- EvenNumbersCounter.Tests/EvenNumbersCounterTests.fs
+++ EvenNumbersCounter.Tests/EvenNumbersCounterTests.fs
@@ -7,18 +7,19 @@ open EvenNumbersCounters
 
 [<Test>]
 let ``MapCounter function counts right`` () =
-      mapCounter [1; 2; 3; 4] |> should equal 2
+    mapCounter [ 1; 2; 3; 4 ] |> should equal 2
 
 [<Test>]
 let ``FoldCounter function counts right`` () =
-      foldCounter [1; 2; 3; 4] |> should equal 2
+    foldCounter [ 1; 2; 3; 4 ] |> should equal 2
 
 [<Test>]
 let ``FilterCounter function counts right`` () =
-      filterCounter [1; 2; 3; 4] |> should equal 2
+    filterCounter [ 1; 2; 3; 4 ] |> should equal 2
 
 [<Test>]
 let ``FilterCounter, mapCounter and foldCounter are equivalent`` () =
-    let countersResultsEq (ls: list<int>) = (mapCounter ls = foldCounter ls) && (mapCounter ls = filterCounter ls)
+    let countersResultsEq (ls: list<int>) =
+        (mapCounter ls = foldCounter ls) && (mapCounter ls = filterCounter ls)
 
     Check.Quick countersResultsEq
