1f935ddb9ae0b43e1098a62d6dd7bd8709030c0d
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-25 04:47:28 +0300

    Отформатировала, поменяла названия переменных, исправила тест

--- LambdaInterpreter.Tests/LambdaInterpreterTest.fs
+++ LambdaInterpreter.Tests/LambdaInterpreterTest.fs
@@ -6,57 +6,82 @@ open LambdaInterpreter
 
 
 [<Test>]
-let ``Terms without redux stay same`` () =
-      let term = Application (Variable ("y"), LambdaAbstraction ("x", Variable ("x"))) //y lx.x
+let ``Terms without redux stay same test`` () =
+    let term = Application(Variable("y"), LambdaAbstraction("x", Variable("x"))) //y lx.x
 
-      betaReduction term |> should equal <| term
-
-[<Test>]
-let ``Correct substitutions`` () =
-     let term = Application
-                    (Application
-                        (LambdaAbstraction ("x",
-                            LambdaAbstraction ("z", Variable ("z"))),
-                                Variable ("y")),
-                                    Variable ("y")) // (lx.lz.z) y y
-
-     let res = betaReduction term
-
-     res |> should equal <| Variable ("y")
+    betaReduction term |> should equal <| term
 
 [<Test>]
-let ``Correct alpha conversion`` () =
-    let term = Application
-                    (LambdaAbstraction
-                        ("x", LambdaAbstraction
-                                ("y", Application
-                                    (Variable ("y"), Variable ("x")))), Variable ("y")) // (lx.ly. y x) y
+let ``Correct substitutions test`` () =
+    let term =
+        Application(
+            Application(LambdaAbstraction("x", LambdaAbstraction("z", Variable("z"))), Variable("y")),
+            Variable("y")
+        ) // (lx.lz.z) y y
 
     let res = betaReduction term
 
-    res |> should equal <| LambdaAbstraction ("y'", Application (Variable ("y'"), Variable ("y")))
+    res |> should equal <| Variable("y")
 
 [<Test>]
-let ``Check S K K = I`` () =
-    let sInternal = Application (Application (Variable ("x"), Variable ("z")), Application (Variable ("y"), Variable ("z")))
-    let s = LambdaAbstraction ("x", LambdaAbstraction ("y", LambdaAbstraction ("z", sInternal)))
-    let k = LambdaAbstraction ("x", LambdaAbstraction ("y", Variable ("x")))
+let ``Correct alpha conversion test`` () =
+    let term =
+        Application(
+            LambdaAbstraction("x", LambdaAbstraction("y", Application(Variable("y"), Variable("x")))),
+            Variable("y")
+        ) // (lx.ly. y x) y
 
-    let term = Application (Application (s, k), k)
-    let i = LambdaAbstraction ("z", Variable ("z"))
+    let res = betaReduction term
+
+    res |> should equal
+    <| LambdaAbstraction("y'", Application(Variable("y'"), Variable("y")))
+
+[<Test>]
+let ``Check S K K = I test`` () =
+    let sInternal =
+        Application(Application(Variable("x"), Variable("z")), Application(Variable("y"), Variable("z")))
+
+    let s =
+        LambdaAbstraction("x", LambdaAbstraction("y", LambdaAbstraction("z", sInternal)))
+
+    let k = LambdaAbstraction("x", LambdaAbstraction("y", Variable("x")))
+
+    let term = Application(Application(s, k), k)
+    let i = LambdaAbstraction("z", Variable("z"))
 
     let res = betaReduction term
 
     res |> should equal <| i
 
 [<Test>]
-let ``Check term reduction`` () =
-    let bbb = LambdaAbstraction ("b", Application( Variable ("b"), Variable ("b")))
-    let bbb2 = Application (bbb, bbb)
-    let ab = LambdaAbstraction("a", bbb2)
-    let sndTerm = Application (LambdaAbstraction ("c", Application ( Variable ("c"), Variable ("b"))), LambdaAbstraction ("a", Variable ("a")))
-    let term = Application (Application (ab, Variable ("b")), sndTerm)
+let ``Check term reduction on fst hw expression test`` () =
+    let term =
+        Application(
+            Application(
+                LambdaAbstraction(
+                    "a",
+                    Application(
+                        LambdaAbstraction("b", Application(Variable("b"), Variable("b"))),
+                        LambdaAbstraction("b", Application(Variable("b"), Variable("b")))
+                    )
+                ),
+                Variable("b")
+            ),
+            Application(
+                LambdaAbstraction("c", Application(Variable("c"), Variable("b"))),
+                LambdaAbstraction("a", Variable("a"))
+            )
+        )
 
     let res = betaReduction term
 
-    res |> should equal <| Application (bbb2, Variable ("b"))
+    let rightResult =
+        Application(
+            Application(
+                LambdaAbstraction("b", Application(Variable("b"), Variable("b"))),
+                LambdaAbstraction("b", Application(Variable("b"), Variable("b")))
+            ),
+            Variable("b")
+        )
+
+    res |> should equal <| rightResult
--- LambdaInterpreter/LambdaInterpreter.fs
+++ LambdaInterpreter/LambdaInterpreter.fs
@@ -7,63 +7,68 @@ module LambdaInterpreter =
         | Application of LambdaTerm * LambdaTerm
         | LambdaAbstraction of string * LambdaTerm
 
-    let rec IsFV term var = 
+    let rec IsFV term var =
         match term with
-            | Variable (x) -> x = var
-            | Application (fstTerm, sndTerm) ->
-                (IsFV fstTerm var) || (IsFV sndTerm var)
-            | LambdaAbstraction (x, internalTerm) ->
-                if x = var then false
-                else IsFV internalTerm var
+        | Variable x -> x = var
+        | Application(fstTerm, sndTerm) -> (IsFV fstTerm var) || (IsFV sndTerm var)
+        | LambdaAbstraction(x, internalTerm) -> if x = var then false else IsFV internalTerm var
 
     let rec alphaConversion term oldVar newVar =
         match term with
-            | Variable (x) -> if x = oldVar then Variable (newVar) else term
-            | Application (fstTerm, sndTerm) ->
-                Application (alphaConversion fstTerm oldVar newVar, alphaConversion sndTerm oldVar newVar)
-            | LambdaAbstraction (x, internalTerm) ->
-                if x = oldVar then
-                    if IsFV internalTerm newVar then term
-                    else LambdaAbstraction (newVar, alphaConversion internalTerm oldVar newVar)
-                else LambdaAbstraction (x, alphaConversion internalTerm oldVar newVar)
-
-    let rec substitution term x substitutionTerm =
-        match term with
-            | Variable (y) -> if x = y then substitutionTerm
-                              else term
-            | Application (fstTerm, sndTerm) ->
-                Application (substitution fstTerm x substitutionTerm, substitution sndTerm x substitutionTerm)
-            | LambdaAbstraction (y, internalTerm) ->
-                if x = y || not (IsFV internalTerm x) then term
+        | Variable x -> if x = oldVar then Variable newVar else term
+        | Application(fstTerm, sndTerm) ->
+            Application(alphaConversion fstTerm oldVar newVar, alphaConversion sndTerm oldVar newVar)
+        | LambdaAbstraction(x, internalTerm) ->
+            if x = oldVar then
+                if IsFV internalTerm newVar then
+                    term
                 else
-                    if IsFV substitutionTerm y then
-                        let newY = (y + "'")
-                        let newInternalTerm = alphaConversion internalTerm y newY
-                        LambdaAbstraction (newY, substitution newInternalTerm x substitutionTerm)
-                    else LambdaAbstraction (y, substitution internalTerm x substitutionTerm) 
-        
+                    LambdaAbstraction(newVar, alphaConversion internalTerm oldVar newVar)
+            else
+                LambdaAbstraction(x, alphaConversion internalTerm oldVar newVar)
+
+    let rec substitution termForChange substitutionVar substitutionTerm =
+        match termForChange with
+        | Variable y ->
+            if substitutionVar = y then
+                substitutionTerm
+            else
+                termForChange
+        | Application(fstTerm, sndTerm) ->
+            Application(
+                substitution fstTerm substitutionVar substitutionTerm,
+                substitution sndTerm substitutionVar substitutionTerm
+            )
+        | LambdaAbstraction(y, internalTerm) ->
+            if substitutionVar = y || not (IsFV internalTerm substitutionVar) then
+                termForChange
+            else if IsFV substitutionTerm y then
+                let newY = y + "'"
+                let newInternalTerm = alphaConversion internalTerm y newY
+                LambdaAbstraction(newY, substitution newInternalTerm substitutionVar substitutionTerm)
+            else
+                LambdaAbstraction(y, substitution internalTerm substitutionVar substitutionTerm)
+
     let rec internalBetaReduction term =
         match term with
-            | Variable _ -> term
-            | LambdaAbstraction (var, internalTerm) ->
-                LambdaAbstraction (var, internalBetaReduction internalTerm)
-            | Application (fstTerm, sndTerm) ->
-                match fstTerm with
-                    | LambdaAbstraction (var, internalTerm) ->
-                        substitution internalTerm var sndTerm
-                    | Variable _ ->
-                        Application (fstTerm, internalBetaReduction sndTerm)
-                    | Application _ ->
-                        let reducedFstTerm  = internalBetaReduction fstTerm 
-                        if reducedFstTerm  = fstTerm then Application (fstTerm, internalBetaReduction sndTerm)
-                        else Application (reducedFstTerm, sndTerm)
+        | Variable _ -> term
+        | LambdaAbstraction(var, internalTerm) -> LambdaAbstraction(var, internalBetaReduction internalTerm)
+        | Application(fstTerm, sndTerm) ->
+            match fstTerm with
+            | LambdaAbstraction(var, internalTerm) -> substitution internalTerm var sndTerm
+            | Variable _ -> Application(fstTerm, internalBetaReduction sndTerm)
+            | Application _ ->
+                let reducedFstTerm = internalBetaReduction fstTerm
+
+                if reducedFstTerm = fstTerm then
+                    Application(fstTerm, internalBetaReduction sndTerm)
+                else
+                    Application(reducedFstTerm, sndTerm)
 
     let rec betaReduction term =
         let reducedTerm = internalBetaReduction term
 
-        if reducedTerm = term then term
-        else betaReduction reducedTerm
-
-        
-    
-
+        if reducedTerm = term then
+            term
+        else
+            betaReduction reducedTerm
