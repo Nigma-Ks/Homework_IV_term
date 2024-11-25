af58742fe7e66fd79199f6d21f341a6094c72b04
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-25 08:30:22 +0300

    Отформатировала

--- Workflow.Tests/WorkflowTests.fs
+++ Workflow.Tests/WorkflowTests.fs
@@ -6,31 +6,35 @@ open Workflow
 
 [<Test>]
 let ``With numbers calculating test`` () =
-    let result = calculate {
-        let! x = "1"
-        let! y = "2"
-        let z = x + y
-        return z
-    }
+    let result =
+        calculate {
+            let! x = "1"
+            let! y = "2"
+            let z = x + y
+            return z
+        }
+
     result |> should equal <| Some(3)
 
 [<Test>]
 let ``With incorrect numbers calculating test`` () =
-    let result = calculate {
-        let! x = "1"
-        let! y = "Ъ"
-        let z = x + y
-        return z
-    }
+    let result =
+        calculate {
+            let! x = "1"
+            let! y = "Ъ"
+            let z = x + y
+            return z
+        }
+
     result |> should equal <| None
 
 [<Test>]
 let ``Rounding test`` () =
-    let result = rounding 3 {
-        let! a = 2.0 / 12.0
-        let! b = 3.5
-        return a / b
-    }
+    let result =
+        rounding 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+
     result |> should equal <| 0.048
-
-
--- Workflow/Workflow.fs
+++ Workflow/Workflow.fs
@@ -4,23 +4,22 @@ open System
 
 module Workflow =
     let toNumber (str: string) =
-            match System.Int32.TryParse str with
-            | true, int -> Some int
-            | _ -> None
+        match System.Int32.TryParse str with
+        | true, int -> Some int
+        | _ -> None
 
     type CalculationBuilder() =
-        member this.Bind(x:string, f) =
-            match toNumber(x) with
-                | None -> None
-                | Some a -> f a
-        member this.Return(x) =
-            Some x
-    let calculate = CalculationBuilder();
+        member this.Bind(x: string, f) =
+            match toNumber (x) with
+            | None -> None
+            | Some a -> f a
+
+        member this.Return(x) = Some x
+
+    let calculate = CalculationBuilder()
 
     type RoundingCalculationBuilder(precision: int) =
-        member this.Bind(x: float, f) =
-            f <| Math.Round(x, precision)
-        member this.Return(x: float) =
-            Math.Round(x, precision)
+        member this.Bind(x: float, f) = f <| Math.Round(x, precision)
+        member this.Return(x: float) = Math.Round(x, precision)
 
     let rounding = RoundingCalculationBuilder
