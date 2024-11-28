34c19be286af50be6c05a00149cf9613a90f6e28
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-11-28 06:34:55 +0300

    Добавила решение доп задачи

---
+++ TreeIterator.Tests/TreeIteratorTests.fs
@@ -0,0 +1,28 @@
+﻿module TreeIterator.Tests
+
+open NUnit.Framework
+open FsUnit
+open TreeIterator
+
+
+let avlTree = AVLTree<int>()
+let values = [ 10; 20; 30; 40; 50; 25 ]
+
+for value in values do
+    avlTree.Insert(value)
+
+[<Test>]
+let ``AVL tree inserts elements right`` () =
+
+    avlTree.Root.Value.Value |> should equal 30
+
+[<Test>]
+let ``Iterator test`` () =
+    let mutable valuesList = []
+    let enumerator = avlTree.GetEnumerator()
+    valuesList <- enumerator.Current :: valuesList
+
+    while enumerator.MoveNext() do
+        valuesList <- enumerator.Current :: valuesList
+
+    valuesList |> should equal [ 50; 40; 25; 10; 20; 30 ]
